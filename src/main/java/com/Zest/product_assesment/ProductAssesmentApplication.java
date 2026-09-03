1b906ba6f5b7b5bb7a3f0746713d60b9b5467b81
yogidas <yogeshvaishnav332@gmail.com>
2026-09-03 08:40:01 +0530

    This is my Project Assesment

---
+++ src/main/java/com/Zest/product_assesment/ProductAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.universal.product_assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductAssesmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Zest/product_assesment/ProductAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package product_assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = com.Zest.product_assesment.ProductAssesmentApplication.class)
+class ProductAssesmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
